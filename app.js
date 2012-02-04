5e19804bd96443ac4c275263bbad1669d26bbe0e
小松 健作 <kensaku.komatsu@gmail.com>
2012-02-04 22:00:49 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , WebSocketServer = require('websocket').server
+  , fs = require('fs')
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+app.get('/logo/:id', function(req, res){
+  fs.readFile('./public/images/html5-32.png', function(e, data) {
+    res.header("Content-Type", "image/png");
+    res.header("Connection", "close");
+    res.send(data);
+    res.end();
+  });
+});
+
+
+// WebSocket
+
+var wsServer = new WebSocketServer({
+  httpServer: app,
+  autoAcceptConnections: false
+});
+
+function checkOrigin(origin){
+  // TODO check origin is valid or not
+  return true;
+}
+
+wsServer.on('request', function(req) {
+  if (!checkOrigin(req.origin)) {
+    req.reject();
+    console.log(req.origin+" is rejected");
+    return;
+  }
+
+  var conn = req.accept('', req.origin);
+
+  conn.on('message', function(mesg) {
+    fs.readFile('./public/images/html5-32.png', function(e, data) {
+      conn.sendBytes(data);
+    });
+  });
+  conn.on('close', function(){});
+});
+
+app.listen(process.env.VMC_APP_PORT || 3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ public/javascripts/wsclient.js
@@ -0,0 +1,78 @@
+var num = 150;
+window.URL = window.URL || window.webkitURL;
+
+$("#num").text(num);
+
+var ws = new WebSocket("ws://"+location.host)
+ws.onopen = function(){
+  $("button.http").fadeIn();
+  $("button.websocket").fadeIn();
+}
+
+$("button.http").click(function(){
+  withHTTP();
+}).hide();
+$("button.websocket").click(function(){
+  withWebSocket();
+}).hide();
+// websocket
+// ----------------------------
+var withWebSocket = function(){
+  var count = 0
+    , out = $('output.websocket')
+    , st
+    , res
+
+  out.empty();
+  $(".res_websocket").empty();
+
+  for(var i = 0; i < num; i += 1){
+    ws.send("get");
+  }
+
+  ws.onmessage = function(e) {
+    var blob = e.data;
+
+    var url = window.URL.createObjectURL(blob);
+    out.append("<img src='"+url+"'>");
+
+    st = (count === 0 ? new Date().getTime() : st);
+    count += 1;
+
+    if(count === num) {
+      $(".res_websocket").text(new Date().getTime() - st);
+    }
+  }
+};
+
+// http
+// -------------------------------
+var withHTTP = function(){
+  var out = $('output.http')
+    , st
+    , res
+    , count = 0;
+
+  var getImg = function(id){
+    out.append("<img id='httptest"+id+"'src='/logo/"+id+"?"+new Date().getTime()+"'>");
+
+
+    $("#httptest"+id).bind("load", function(){
+      count += 1;
+      if(count === num) {
+        $(".res_http").text(new Date().getTime() - st);
+      }
+    });
+  }
+
+  var start = function(){
+    out.empty();
+    $(".res_http").empty();
+
+    st = new Date().getTime();
+    for(var i = 0; i < num; i += 1){
+      getImg(i);
+    };
+  }
+  start();
+};
